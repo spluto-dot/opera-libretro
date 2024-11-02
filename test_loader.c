4374ae94ba21dd063ffa820f04543fd185625986
spluto-dot <brunoraji@ymail.com>
2024-11-02 01:29:52 -0300

    Update test_loader.c

--- test_loader.c
+++ test_loader.c
@@ -1,5 +1,5 @@
-#include <windows.h>
 #include <stdio.h>
+#include <windows.h>
 #include <stdint.h>
 
 typedef void (*lr_input_device_set_func)(const uint32_t, const uint32_t);
@@ -11,14 +11,20 @@ int main() {
         return 1;
     }
 
-    lr_input_device_set_func lr_input_device_set = (lr_input_device_set_func)GetProcAddress(dll, "lr_input_device_set");
+    lr_input_device_set_func lr_input_device_set = 
+        (lr_input_device_set_func)GetProcAddress(dll, "lr_input_device_set");
+    
     if (!lr_input_device_set) {
-        printf("Function not found in DLL\n");
+        printf("Function 'lr_input_device_set' not found in DLL\n");
         FreeLibrary(dll);
         return 1;
     }
 
-    lr_input_device_set(0, 1);
+    // Chamada da função com valores de teste e confirmação visual
+    printf("Calling 'lr_input_device_set' with port = 1, device = 1\n");
+    lr_input_device_set(1, 1);
+    printf("Function 'lr_input_device_set' called successfully\n");
+
     FreeLibrary(dll);
     return 0;
 }
