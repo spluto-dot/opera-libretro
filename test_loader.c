47c44327d92c06ae703cffee4b10fbc0e0fa3671
spluto-dot <brunoraji@ymail.com>
2024-11-02 00:25:05 -0300

    Add files via upload

---
+++ test_loader.c
@@ -0,0 +1,24 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdint.h>
+
+typedef void (*lr_input_device_set_func)(const uint32_t, const uint32_t);
+
+int main() {
+    HINSTANCE dll = LoadLibrary("opera_libretro.dll");
+    if (!dll) {
+        printf("Failed to load DLL\n");
+        return 1;
+    }
+
+    lr_input_device_set_func lr_input_device_set = (lr_input_device_set_func)GetProcAddress(dll, "lr_input_device_set");
+    if (!lr_input_device_set) {
+        printf("Function not found in DLL\n");
+        FreeLibrary(dll);
+        return 1;
+    }
+
+    lr_input_device_set(0, 1);
+    FreeLibrary(dll);
+    return 0;
+}
