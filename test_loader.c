deb443f91d8fb545aa0e9809f5873ad69a41f12a
spluto-dot <brunoraji@ymail.com>
2024-11-01 23:38:46 -0300

    Update test_loader.c

--- test_loader.c
+++ test_loader.c
@@ -4,14 +4,12 @@
 typedef void (*lr_input_device_set_func)(const uint32_t, const uint32_t);
 
 int main() {
-    // Carrega a DLL
     HINSTANCE dll = LoadLibrary("opera_libretro.dll");
     if (!dll) {
         printf("Failed to load DLL\n");
         return 1;
     }
 
-    // Obtém o ponteiro para a função lr_input_device_set
     lr_input_device_set_func lr_input_device_set = (lr_input_device_set_func)GetProcAddress(dll, "lr_input_device_set");
     if (!lr_input_device_set) {
         printf("Function not found in DLL\n");
@@ -19,10 +17,7 @@ int main() {
         return 1;
     }
 
-    // Chama a função para testar e ver a saída
-    lr_input_device_set(0, 1);  // Passa os valores 0 e 1 para a função
-    
-    // Libera a DLL
+    lr_input_device_set(0, 1);  // Teste da função
     FreeLibrary(dll);
     return 0;
 }
